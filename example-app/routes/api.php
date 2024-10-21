f36ca4323ed644f98c8a76a532dfc04061a24433
Sneha Neupane <snehaneup@gmail.com>
2024-10-21 10:12:35 +0545

    Second

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::post('/products',
+// [ProductController::class, 'store']);
+
+Route::resource('products', ProductController::class);
+
+Route::get('/products/search/{name}', [ProductController::class, 'search']);
